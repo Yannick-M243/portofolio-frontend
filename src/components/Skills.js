be46ad0083d9e7ef9830e893846f27589c36c651
Yannick Makwenge <yannickmakwenge@gmail.com>
2022-12-23 20:41:06 +0100

    fix spelling

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -47,7 +47,7 @@ function Skills() {
         return (
             <section id="skills">
                 <h2>Skills</h2>
-                <h3>This are all the technologies I can work with</h3>
+                <h3>These are all the technologies I can work with</h3>
                 <div className='container'>
                     <img src={cssLogo} alt="css" data-aos="flip-left"></img>
                     <img src={sassLogo} alt="sass" className='sass' data-aos="flip-left"></img>
