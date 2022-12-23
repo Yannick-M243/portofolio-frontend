24c1016af06d423b1b8e3da51f52d5c6d39eb78a
Yannick Makwenge <yannickmakwenge@gmail.com>
2022-12-23 20:05:13 +0100

    disabling skills animation on mobile

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -20,20 +20,20 @@ function Skills() {
             <h2>Skills</h2>
             <h3>This are all the technologies I can work with</h3>
             <div className='container'>
-                <img src={cssLogo} alt="css" data-aos="flip-left"></img>
-                <img src={sassLogo} alt="sass" className='sass' data-aos="flip-left"></img>
-                <img src={htmlLogo} alt="html" data-aos="flip-left"></img>
-                <img src={figmaLogo} alt="figma" data-aos="flip-left"></img>
-                <img src={laravelLogo} alt="laravel" data-aos="flip-left"></img>
-                <img src={expressLogo} alt="express" data-aos="flip-left" className='express'></img>
-                <img src={javaLogo} alt="java" data-aos="flip-left"></img>
-                <img src={javascriptLogo} alt="javascript" data-aos="flip-left"></img>
-                <img src={mongoLogo} alt="mongo" className='mongo' data-aos="flip-left"></img>
-                <img src={mysqlLogo} alt="mysql" data-aos="flip-left"></img>
-                <img src={phpLogo} alt="php" className='php' data-aos="flip-left"></img>
-                <img src={reactLogo} alt="react" data-aos="flip-left"></img>
-                <img src={nodeLogo} alt="node" data-aos="flip-left"></img>
-                <img src={sqlLogo} alt="node" data-aos="flip-left"></img>
+                <img src={cssLogo} alt="css" data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={sassLogo} alt="sass" className='sass' data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={htmlLogo} alt="html" data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={figmaLogo} alt="figma" data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={laravelLogo} alt="laravel" data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={expressLogo} alt="express" data-aos="flip-left" className='express' data-aos-disable='mobile'></img>
+                <img src={javaLogo} alt="java" data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={javascriptLogo} alt="javascript" data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={mongoLogo} alt="mongo" className='mongo' data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={mysqlLogo} alt="mysql" data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={phpLogo} alt="php" className='php' data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={reactLogo} alt="react" data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={nodeLogo} alt="node" data-aos="flip-left" data-aos-disable='mobile'></img>
+                <img src={sqlLogo} alt="node" data-aos="flip-left" data-aos-disable='mobile'></img>
             </div>
         </section>
     );
