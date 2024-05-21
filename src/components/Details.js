b97a5c613bcfd04daed477f10b07c93f03d04d27
Yannick Makwenge <yannickmakwenge@gmail.com>
2024-05-21 10:13:11 +0200

    style: improve details section on mobile

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,10 +7,10 @@ function Details() {
                 <section id="details">
                         <h2>Details</h2>
                         <div className='container'>
-                                <p><span className="primary-font"><FontAwesomeIcon icon={faHouseFlag} /> Nationality : </span> Congolese</p>
-                                <p><span className="primary-font"><FontAwesomeIcon icon={faMapPin} /> Location : </span> Lubumbashi, DR Congo</p>
-                                <p><span className="primary-font"><FontAwesomeIcon icon={faEnvelope} /> Email Address : </span> Yannickmakwenge@gmail.com</p>
-                                <p><span className="primary-font"><FontAwesomeIcon icon={faDumbbell} /> Hobbies : </span> Fitness / Gaming / Anime</p>
+                                <p><span className="primary-font"><FontAwesomeIcon icon={faHouseFlag} /> Nationality: </span>Congolese</p>
+                                <p><span className="primary-font"><FontAwesomeIcon icon={faMapPin} /> Location: </span>Lubumbashi, DR Congo</p>
+                                <p><span className="primary-font"><FontAwesomeIcon icon={faEnvelope} /> Email: </span>yannickmakwenge@gmail.com</p>
+                                <p><span className="primary-font"><FontAwesomeIcon icon={faDumbbell} /> Hobbies: </span>Fitness|Gaming|Anime</p>
                         </div>
                 </section>
         );
