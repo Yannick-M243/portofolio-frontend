555ee1248492fdfdaf2a5269b378501cae1d6768
Yannick Makwenge <yannickmakwenge@gmail.com>
2024-09-13 12:03:18 +0200

    remove CV

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import CV from '../docs/CV-Yannick-Boteti-Makwenge-2024.pdf';
 
 function Footer() {
   return (
       <section id="footer">
           <p>Want more details?</p> 
-          <a href={CV} download="CV-Yannick-Boteti-Makwenge-2024"
-              target="_blank" rel="noreferrer">
-              <div className='link-container'>Download my CV</div>
+          <a href="mailto:yannickmakwenge@gmail.com" rel="noreferrer">
+              <div className='link-container'>Get in touch</div>
           </a> 
           <div className='outer-footer'>
               <span className='copyright'>&copy;2024 Copyright <a href="https://github.com/Yannick-M243" target="_blank" rel="noreferrer">Yannick Makwenge.</a></span>
